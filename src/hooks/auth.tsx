d1d4740d9179b30d6f3c44b4b4163305bd79fcc8
Bruno Barbosa <barbosa.developer@gmail.com>
2020-09-04 01:28:08 -0300

    Configuração inicial, autenticação e tela de login

---
+++ src/hooks/auth.tsx
@@ -0,0 +1,93 @@
+import React, { createContext, useCallback, useState, useContext } from 'react';
+import api from '../services/api';
+
+interface User {
+  id: string;
+  name: string;
+  permission: string;
+  avatar_url: string;
+}
+
+interface AuthState {
+  token: string;
+  user: User;
+}
+
+interface SignInCredentials {
+  name: string;
+  password: string;
+}
+
+interface AuthContextData {
+  user: User;
+  signIn(credentials: SignInCredentials): Promise<void>;
+  signOut(): void;
+  updateUser(user: User): void;
+}
+
+const AuthContext = createContext<AuthContextData>({} as AuthContextData);
+
+const AuthProvider: React.FC = ({ children }) => {
+  const [data, setData] = useState<AuthState>(() => {
+    const token = localStorage.getItem('@Brasucas.token');
+    const user = localStorage.getItem('@Brasucas.user');
+
+    if (token && user) {
+      api.defaults.headers.authorization = `Bearer ${token}`;
+      return { token, user: JSON.parse(user) };
+    }
+
+    return {} as AuthState;
+  });
+
+  const signIn = useCallback(async ({ name, password }) => {
+    const response = await api.post('sessions', { name, password });
+
+    const { token, user } = response.data;
+    localStorage.setItem('@Brasucas.token', token);
+    localStorage.setItem('@Brasucas.user', JSON.stringify(user));
+
+    api.defaults.headers.authorization = `Bearer ${token}`;
+
+    setData({ token, user });
+  }, []);
+
+  const signOut = useCallback(() => {
+    localStorage.removeItem('@Brasucas.token');
+    localStorage.removeItem('@Brasucas.user');
+
+    setData({} as AuthState);
+  }, []);
+
+  const updateUser = useCallback(
+    (user: User) => {
+      localStorage.setItem('@Brasucas.user', JSON.stringify(user));
+
+      setData({
+        token: data.token,
+        user,
+      });
+    },
+    [setData, data.token],
+  );
+
+  return (
+    <AuthContext.Provider
+      value={{ user: data.user, signIn, signOut, updateUser }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+function useAuth(): AuthContextData {
+  const context = useContext(AuthContext);
+
+  if (!context) {
+    throw new Error('useAuth must be used within an AuthProvider');
+  }
+
+  return context;
+}
+
+export { AuthProvider, useAuth };
---
+++ src/pages/Signin/index.tsx
@@ -0,0 +1,86 @@
+import React, { useRef, useCallback } from 'react';
+import { FiUser, FiLock } from 'react-icons/fi';
+import { FormHandles } from '@unform/core';
+import { Form } from '@unform/web';
+import * as Yup from 'yup';
+import { useHistory } from 'react-router-dom';
+import { Container, Content, Background, AnimationCotainer } from './styles';
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+import getValidationErrors from '../../utils/getValidationErrors';
+import { useAuth } from '../../hooks/auth';
+import { useToast } from '../../hooks/toast';
+
+import logoImg from '../../assets/logo.svg';
+
+interface SignInFormData {
+  name: string;
+  password: string;
+}
+
+const Signin: React.FC = () => {
+  const formRef = useRef<FormHandles>(null);
+
+  const { signIn } = useAuth();
+  const { addToast } = useToast();
+  const history = useHistory();
+
+  const handleSubmit = useCallback(
+    async (data: SignInFormData) => {
+      try {
+        formRef.current?.setErrors({});
+
+        const schema = Yup.object().shape({
+          name: Yup.string().required('Nome obrigatório'),
+          password: Yup.string().required('Senha obrigatória'),
+        });
+        await schema.validate(data, {
+          abortEarly: false,
+        });
+
+        await signIn({ name: data.name, password: data.password });
+
+        history.push('/dashboard');
+      } catch (err) {
+        if (err instanceof Yup.ValidationError) {
+          const errors = getValidationErrors(err);
+          formRef.current?.setErrors(errors);
+          return;
+        }
+
+        addToast({
+          type: 'error',
+          title: 'Erro na autenticação',
+          description: 'Ocorreu um erro ao fazer login, cheque as credenciais',
+        });
+      }
+    },
+    [signIn, addToast, history],
+  );
+
+  return (
+    <Container>
+      <Content>
+        <AnimationCotainer>
+          <img src={logoImg} alt="Brasucas" />
+
+          <Form ref={formRef} onSubmit={handleSubmit}>
+            <h1>Faça seu logon</h1>
+
+            <Input name="name" icon={FiUser} placeholder="Nome da Família" />
+            <Input
+              name="password"
+              icon={FiLock}
+              placeholder="Senha"
+              type="password"
+            />
+            <Button type="submit">Entrar</Button>
+          </Form>
+        </AnimationCotainer>
+      </Content>
+      <Background />
+    </Container>
+  );
+};
+
+export default Signin;
